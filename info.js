30e9603281ac48f861841e15f259ff1bec2506b7
EthanPurdue <etbusha@purdue.edu>
2024-11-24 18:20:30 -0500

    Add files via upload

---
+++ info.js
@@ -0,0 +1,276 @@
+// THIS ARRAY IS AI GENERATED
+//  I created the attributes I wanted but I had ai create a list filling in object examples
+
+
+const cars = [
+    {
+        carMake: "Toyota",
+        carModel: "Corolla",
+        carType: "Sedan",
+        carColor: "Blue",
+        carYear: 2022,
+        carMiles: 12000,
+        carPrice: 20000
+    },
+    {
+        carMake: "Ford",
+        carModel: "Ranger",
+        carType: "Truck",
+        carColor: "Red",
+        carYear: 2021,
+        carMiles: 25000,
+        carPrice: 28000
+    },
+    {
+        carMake: "Chevrolet",
+        carModel: "Tahoe",
+        carType: "SUV",
+        carColor: "Black",
+        carYear: 2020,
+        carMiles: 30000,
+        carPrice: 35000
+    },
+    {
+        carMake: "Tesla",
+        carModel: "Model S",
+        carType: "Luxury",
+        carColor: "White",
+        carYear: 2023,
+        carMiles: 5000,
+        carPrice: 95000
+    },
+    {
+        carMake: "BMW",
+        carModel: "X5",
+        carType: "SUV",
+        carColor: "Gray",
+        carYear: 2019,
+        carMiles: 40000,
+        carPrice: 45000
+    },
+    {
+        carMake: "Porsche",
+        carModel: "911",
+        carType: "Luxury",
+        carColor: "Yellow",
+        carYear: 2022,
+        carMiles: 8000,
+        carPrice: 120000
+    },
+    {
+        carMake: "Honda",
+        carModel: "Accord",
+        carType: "Sedan",
+        carColor: "Silver",
+        carYear: 2021,
+        carMiles: 18000,
+        carPrice: 25000
+    },
+    {
+        carMake: "Ram",
+        carModel: "1500",
+        carType: "Truck",
+        carColor: "Blue",
+        carYear: 2020,
+        carMiles: 35000,
+        carPrice: 38000
+    },
+    {
+        carMake: "Nissan",
+        carModel: "Altima",
+        carType: "Sedan",
+        carColor: "Green",
+        carYear: 2018,
+        carMiles: 50000,
+        carPrice: 15000
+    },
+    {
+        carMake: "Ford",
+        carModel: "F-150",
+        carType: "Truck",
+        carColor: "Silver",
+        carYear: 2017,
+        carMiles: 60000,
+        carPrice: 25000
+    },
+    {
+        carMake: "Chevrolet",
+        carModel: "Equinox",
+        carType: "SUV",
+        carColor: "Red",
+        carYear: 2019,
+        carMiles: 35000,
+        carPrice: 23000
+    },
+    {
+        carMake: "Hyundai",
+        carModel: "Tucson",
+        carType: "SUV",
+        carColor: "Black",
+        carYear: 2018,
+        carMiles: 45000,
+        carPrice: 22000
+    },
+    {
+        carMake: "BMW",
+        carModel: "3 Series",
+        carType: "Luxury",
+        carColor: "White",
+        carYear: 2015,
+        carMiles: 80000,
+        carPrice: 17000
+    },
+    {
+        carMake: "Audi",
+        carModel: "A6",
+        carType: "Luxury",
+        carColor: "Blue",
+        carYear: 2020,
+        carMiles: 25000,
+        carPrice: 45000
+    },
+    {
+        carMake: "Mazda",
+        carModel: "CX-5",
+        carType: "SUV",
+        carColor: "Orange",
+        carYear: 2021,
+        carMiles: 12000,
+        carPrice: 29000
+    },
+    {
+        carMake: "Honda",
+        carModel: "Civic",
+        carType: "Sedan",
+        carColor: "Yellow",
+        carYear: 2017,
+        carMiles: 70000,
+        carPrice: 12000
+    },
+    {
+        carMake: "Kia",
+        carModel: "Sorento",
+        carType: "SUV",
+        carColor: "Purple",
+        carYear: 2019,
+        carMiles: 32000,
+        carPrice: 24000
+    },
+    {
+        carMake: "Toyota",
+        carModel: "Highlander",
+        carType: "SUV",
+        carColor: "Gray",
+        carYear: 2021,
+        carMiles: 22000,
+        carPrice: 37000
+    },
+    {
+        carMake: "Chevrolet",
+        carModel: "Silverado",
+        carType: "Truck",
+        carColor: "Pink",
+        carYear: 2016,
+        carMiles: 80000,
+        carPrice: 27000
+    },
+    {
+        carMake: "Subaru",
+        carModel: "Outback",
+        carType: "SUV",
+        carColor: "Brown",
+        carYear: 2017,
+        carMiles: 70000,
+        carPrice: 22000
+    },
+    {
+        carMake: "Mercedes",
+        carModel: "Benz",
+        carType: "Luxury",
+        carColor: "Silver",
+        carYear: 2018,
+        carMiles: 30000,
+        carPrice: 55000
+    },
+    {
+        carMake: "Ram",
+        carModel: "2500",
+        carType: "Truck",
+        carColor: "Red",
+        carYear: 2020,
+        carMiles: 18000,
+        carPrice: 38000
+    },
+    {
+        carMake: "Toyota",
+        carModel: "Tacoma",
+        carType: "Truck",
+        carColor: "White",
+        carYear: 2016,
+        carMiles: 90000,
+        carPrice: 23000
+    },
+    {
+        carMake: "Honda",
+        carModel: "Pilot",
+        carType: "SUV",
+        carColor: "Silver",
+        carYear: 2021,
+        carMiles: 11000,
+        carPrice: 35000
+    },
+    {
+        carMake: "Ford",
+        carModel: "Explorer",
+        carType: "SUV",
+        carColor: "Black",
+        carYear: 2020,
+        carMiles: 20000,
+        carPrice: 38000
+    },
+    {
+        carMake: "Hyundai",
+        carModel: "Santa Fe",
+        carType: "SUV",
+        carColor: "Gray",
+        carYear: 2020,
+        carMiles: 27000,
+        carPrice: 31000
+    },
+    {
+        carMake: "Chrysler",
+        carModel: "Pacifica",
+        carType: "SUV",
+        carColor: "Blue",
+        carYear: 2019,
+        carMiles: 25000,
+        carPrice: 28000
+    },
+    {
+        carMake: "Chevrolet",
+        carModel: "Camaro",
+        carType: "Luxury",
+        carColor: "Yellow",
+        carYear: 2022,
+        carMiles: 15000,
+        carPrice: 42000
+    },
+    {
+        carMake: "Jaguar",
+        carModel: "F-Type",
+        carType: "Luxury",
+        carColor: "Red",
+        carYear: 2021,
+        carMiles: 12000,
+        carPrice: 65000
+    },
+    {
+        carMake: "Mazda",
+        carModel: "3",
+        carType: "Sedan",
+        carColor: "Teal",
+        carYear: 2015,
+        carMiles: 85000,
+        carPrice: 12000
+    }
+];
